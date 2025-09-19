09340cb2899e515b4909ad1bca457c7486dda373
govindrajpootecosoul <Govind.Kumar@ecosoulhome.com>
2025-09-19 22:09:44 +0530

    update port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./src/app');
 const connectDB = require('./src/config/db');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 3100;
 
 // Connect to MongoDB
 connectDB();
